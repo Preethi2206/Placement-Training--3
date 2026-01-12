b7a438a48ddbb8dc9da297becdceb827913d3de9
Preethi2206 <147687615+Preethi2206@users.noreply.github.com>
2026-01-12 19:10:04 +0530

    Tower of hanoi.py

---
+++ 12.01.2026/Tower of hanoi.py
@@ -0,0 +1,10 @@
+def hanoi(n, src, aux, dest):
+    if n == 1:
+        print(f"Move disk 1 from {src} to {dest}")
+        return
+    hanoi(n - 1, src, dest, aux)
+    print(f"Move disk {n} from {src} to {dest}")
+    hanoi(n - 1, aux, src, dest)
+
+n = int(input())
+hanoi(n, 'A', 'B', 'C')
